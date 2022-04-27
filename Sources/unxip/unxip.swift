e331b3f52227fa4a386a56c0f76d09684d833f69
Brandon Kurtz <brandon.kurtz@airbnb.com>
2022-04-27 15:31:13 -0700

    swift package

---
+++ Sources/unxip/unxip.swift
@@ -0,0 +1,6 @@
+public struct unxip {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/unxipTests/unxipTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import unxip
+
+final class unxipTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(unxip().text, "Hello, World!")
+    }
+}
